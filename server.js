5e36eef594fe5f6536ae44fa02cb67b554f8a5af
Lebogang <lebogangodeveloper@gmail.com>
2023-12-04 12:39:15 +0200

    Update the app to listen to port environment - variable provided by Heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Enable CORS and use bodyParser for JSON parsing
 app.use(cors());
