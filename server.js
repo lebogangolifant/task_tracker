bf6dc7183fbbc1d5c9ecd4b7be7b9a156f1a3173
Lebogang <lebogangodeveloper@gmail.com>
2023-12-04 15:25:22 +0200

    Check if environment variable is set to production

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //  Load the environment variables using dotenv
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const express = require('express');
 const bodyParser = require('body-parser');
