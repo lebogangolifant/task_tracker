36cc3b0b57399dbcad3bbf3c5de2080c8a84abff
Lebogang <lebogangodeveloper@gmail.com>
2023-12-04 13:05:35 +0200

    Update server port

--- server.js
+++ server.js
@@ -119,5 +119,5 @@ app.delete('/tasks/:taskId', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on ${port}`);
 });
